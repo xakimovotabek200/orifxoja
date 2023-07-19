b6d54939dde765c644f9b5f11880a8fc9a55d70f
AbdusamadDev <abdusamaddev571@gmail.com>
2023-07-20 01:10:31 +0500

    agniaegeeg

--- src/components/pageProps/productDetails/ProductInfo.js
+++ src/components/pageProps/productDetails/ProductInfo.js
@@ -9,12 +9,7 @@ const ProductInfo = ({ productInfo }) => {
       <h2 className="text-4xl font-semibold">{productInfo.productName}</h2>
       <p className="text-xl font-semibold">${productInfo.price}</p>
       <p className="text-base text-gray-600">{productInfo.des}</p>
-<<<<<<< HEAD
-      <p className="text-sm">
-Birinchi bo'lib sharh qoldiring.</p>
-=======
       <p className="text-sm">Birinchi bo'lib sharh qoldiring</p>
->>>>>>> 3d7e66db397859b270454382f516cb1364fafe90
       <p className="font-medium text-lg">
         <span className="font-normal">Ranglar:</span> {productInfo.color}
       </p>
@@ -34,19 +29,11 @@ Birinchi bo'lib sharh qoldiring.</p>
         }
         className="w-full py-4 bg-primeColor hover:bg-black duration-300 text-white text-lg font-titleFont"
       >
-<<<<<<< HEAD
-        Savatchaga qo'shish
-      </button>
-      <p className="font-normal text-sm">
-        <span className="text-base font-medium"> Kategoriyalar:</span> Bahor
-        to'plam, Streetwear, Ayollar Teglar: xususiyatli SKU: N/A
-=======
         Savatga qo'shish
       </button>
       <p className="font-normal text-sm">
         <span className="text-base font-medium"> Turlari:</span> Baxorgi
         Kolleksiyalr turkiyadan kelgan
->>>>>>> 3d7e66db397859b270454382f516cb1364fafe90
       </p>
     </div>
   );
