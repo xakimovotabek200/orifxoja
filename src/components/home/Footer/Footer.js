19f3e35f856c1701166bbd92bb48e3e495bd1bea
xakimovoatbek200 <xakimovoatbek@gmail.com>
2023-07-19 21:44:12 +0500

    add fronta

---
+++ src/components/home/Footer/Footer.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { FaFacebook, FaGithub, FaLinkedin, FaYoutube } from "react-icons/fa";
+import FooterListTitle from "./FooterListTitle";
+
+const Footer = () => {
+  const [emailInfo, setEmailInfo] = useState("");
+  const [subscription, setSubscription] = useState(false);
+  const [errMsg, setErrMsg] = useState("");
+
+  const emailValidation = () => {
+    return String(emailInfo)
+      .toLocaleLowerCase()
+      .match(/^\w+([-]?\w+)*@\w+([-]?\w+)*(\.\w{2,3})+$/);
+  };
+
+  const handleSubscription = () => {
+    if (emailInfo === "") {
+      setErrMsg("Please provide an Email !");
+    } else if (!emailValidation(emailInfo)) {
+      setErrMsg("Please give a valid Email!");
+    } else {
+      setSubscription(true);
+      setErrMsg("");
+      setEmailInfo("");
+    }
+  };
+  return (
+    <div className="w-full bg-[#F5F5F3] py-20">
+      <div className="max-w-container mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-6 px-4 gap-10">
+        <div className="col-span-2">
+          <FooterListTitle title="Legend ishonchli tanlov" />
+          <div className="flex flex-col gap-6">
+            <p className="text-base w-full xl:w-[80%]">
+          
+            </p>
+            <ul className="flex items-center gap-2">
+              <a
+                href="https://www.youtube.com/@reactjsBD"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <li className="w-7 h-7 bg-primeColor text-gray-100 hover:text-white cursor-pointer text-lg rounded-full flex justify-center items-center hover:bg-black duration-300">
+                  <FaYoutube />
+                </li>
+              </a>
+              <a
+                href="https://github.com/noorjsdivs"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <li className="w-7 h-7 bg-primeColor text-gray-100 hover:text-white cursor-pointer text-lg rounded-full flex justify-center items-center hover:bg-black duration-300">
+                  <FaGithub />
+                </li>
+              </a>
+              <a
+                href="https://www.facebook.com/Noorlalu143/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <li className="w-7 h-7 bg-primeColor text-gray-100 hover:text-white cursor-pointer text-lg rounded-full flex justify-center items-center hover:bg-black duration-300">
+                  <FaFacebook />
+                </li>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/noor-mohammad-ab2245193/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <li className="w-7 h-7 bg-primeColor text-gray-100 hover:text-white cursor-pointer text-lg rounded-full flex justify-center items-center hover:bg-black duration-300">
+                  <FaLinkedin />
+                </li>
+              </a>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <FooterListTitle title="Sotuv Bo'limi" />
+          <ul className="flex flex-col gap-2">
+            <li className="font-titleFont text-base text-lightText hover:text-black hover:underline decoration-[1px] decoration-gray-500 underline-offset-2 cursor-pointer duration-300">
+              Sumka
+            </li>
+            <li className="font-titleFont text-base text-lightText hover:text-black hover:underline decoration-[1px] decoration-gray-500 underline-offset-2 cursor-pointer duration-300">
+              Forma
+            </li>
+            <li className="font-titleFont text-base text-lightText hover:text-black hover:underline decoration-[1px] decoration-gray-500 underline-offset-2 cursor-pointer duration-300">
+              Futbolka
+            </li>
+            <li className="font-titleFont text-base text-lightText hover:text-black hover:underline decoration-[1px] decoration-gray-500 underline-offset-2 cursor-pointer duration-300">
+              Ryugzak
+            </li>
+            <li className="font-titleFont text-base text-lightText hover:text-black hover:underline decoration-[1px] decoration-gray-500 underline-offset-2 cursor-pointer duration-300">
+              Oyoq kiyimlar
+            </li>
+          </ul>
+        </div>
+        <div>
+          <FooterListTitle title="Accountingiz" />
+          <ul className="flex flex-col gap-2">
+            
+            <li className="font-titleFont text-base text-lightText hover:text-black hover:underline decoration-[1px] decoration-gray-500 underline-offset-2 cursor-pointer duration-300">  Buyurmalaringiz
+            </li>
+            
+          </ul>
+        </div>
+        <div>
+          <FooterListTitle title="Accountingiz" />
+          sadnkk
+        </div>
+      
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
