34bef7e93d442a2a78c12d554ea3848a3512ee05
ibrohim5009 <ibrohimabdurayimov5@gmail.com>
2023-07-20 09:43:06 +0500

    ,frigjig

--- src/components/pageProps/Breadcrumbs.js
+++ src/components/pageProps/Breadcrumbs.js
@@ -12,7 +12,7 @@ const Breadcrumbs = ({ prevLocation, title }) => {
   return (
     <div className="w-full py-10 xl:py-10 flex flex-col gap-3">
       <h1 className="text-5xl text-primeColor font-titleFont font-bold">
-        Aloqa
+      {title}
       </h1>
       <p className="text-sm font-normal text-lightText capitalize flex items-center">
         <span> {prevLocation === "" ? "Asosiy" : prevLocation}</span>
@@ -21,7 +21,7 @@ const Breadcrumbs = ({ prevLocation, title }) => {
           <HiOutlineChevronRight />
         </span>
         <span className="capitalize font-semibold text-primeColor">
-         Aloqa
+        {locationPath}
         </span>
       </p>
     </div>
